7fdcc293cc372b96e74c83acc16993ae5123e7cf
Filippo Diotalevi <filippo.diotalevi@gmail.com>
2011-11-22 21:01:02 +0000

    temporarily deleted comment

--- contents/collections.rb
+++ contents/collections.rb
@@ -3,7 +3,7 @@ class Projects
   def initialize
     @projects = []
     lsa = Project.new('London Street Art')
-    lsa.add('http://photos.diotalevi.com.s3.amazonaws.com/lst/1.jpg', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.')
+    lsa.add('http://photos.diotalevi.com.s3.amazonaws.com/lst/1.jpg')
     (2..10).each do |n|
       lsa.add "http://photos.diotalevi.com.s3.amazonaws.com/lst/#{n}.jpg"      
     end
